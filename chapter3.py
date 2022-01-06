be5786dd0a94d7ca494009ef0ab503bc99441c3a
Shreyash Shinkar <shreyash.shinkar04@gmail.com>
2022-01-06 23:12:47 +0530

    Chapter 3 done

--- chapter3.py
+++ chapter3.py
@@ -1,4 +1,5 @@
 # In OpenCV the X-axis remains the same, but the Y-axis is towards the south
+# For a computer, an image is nothing but a matrix or an array of pixels
 
 from cv2 import cv2
 import numpy as np
@@ -6,10 +7,12 @@ import numpy as np
 img = cv2.imread("Resources/lambo.png")
 print(img.shape)    # Height, Width, Channels(BGR)
 
-imgResize = cv2.resize(img, (311, 231))
+imgResize = cv2.resize(img, (311, 231)) # src, (width, height)
 print(imgResize.shape)
+
+imgCropped = img[0:231, 311:623]  # (height - h1:h2, width - w1:w2)
 
 cv2.imshow("Image", img)
 cv2.imshow("Resized Image", imgResize)
-
+cv2.imshow("Cropped Image", imgCropped)
 cv2.waitKey(0)
