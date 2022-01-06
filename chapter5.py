bf1dbe397e79a3701649b6166d0d419c5e8a0970
Shreyash Shinkar <shreyash.shinkar04@gmail.com>
2022-01-06 23:45:50 +0530

    Modified chapter5.py

---
+++ chapter5.py
@@ -0,0 +1,15 @@
+from cv2 import cv2
+import numpy as np
+
+img = cv2.imread("Resources/cards.jpg")
+
+width, height = 250, 350
+# These points can be found using MS Paint by hovering the mouse over the wanted points
+pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+imgOutput = cv2.warpPerspective(img, matrix, (width,height))
+
+cv2.imshow("Image", img)
+cv2.imshow("Output", imgOutput)
+cv2.waitKey(0)
