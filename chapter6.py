a6f3887ba77d5038c08057d8a02eed556a0b4674
Shreyash Shinkar <shreyash.shinkar04@gmail.com>
2022-01-07 23:39:50 +0530

    Modified chapter6.py

--- chapter6.py
+++ chapter6.py
@@ -4,8 +4,14 @@ import numpy as np
 
 img = cv2.imread("Resources/lena.png")
 
-imgHor = np.hstack((img, img))
 
-cv2.imshow("Horizontal", imgHor)
+# One of the issues with stacking is that the images cannot be resized.
+# Images also need to have the same number of channels to be stacked. Greyscale and BGR images cannot be stacked.
+# We define a function to overcome these difficulties.
 
+# imgHor = np.hstack((img, img))  # Horizontal stack
+# imgVer = np.vstack((img, img))  # Vertical stack
+
+# cv2.imshow("Horizontal", imgHor)
+# cv2.imshow("Vertical", imgVer)
 cv2.waitKey(0)
