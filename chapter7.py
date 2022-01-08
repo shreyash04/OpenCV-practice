993809515489ce032aa20703d0bd2e6529694466
Shreyash Shinkar <shreyash.shinkar04@gmail.com>
2022-01-08 14:20:52 +0530

    Modified chapter7.py

--- chapter7.py
+++ chapter7.py
@@ -3,10 +3,12 @@
 from cv2 import cv2
 import numpy as np
 
-path = "Resources/lambo.png"
+path = "Resources/lambo.png" # Our task is to detect the orange colour in the picture
 img = cv2.imread(path)
+imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-
+# First we will convert the image into the HSV space
 
 cv2.imshow("Original", img)
+cv2.imshow("HSV Image", imgHSV)
 cv2.waitKey(0)
