5af46b39fc9ee97e192f321f79ef2f69d6e8e283
Shreyash Shinkar <shreyash.shinkar04@gmail.com>
2022-01-08 15:56:30 +0530

    Finished chapter8.py

--- chapter8.py
+++ chapter8.py
@@ -46,7 +46,26 @@ def getContours(img):
             print(peri)
             approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
             print(len(approx))
+            objCor = len(approx)
+            x, y , w, h = cv2.boundingRect(approx)
 
+            if objCor == 3:
+                objectType = "Tri"
+            elif objCor == 4:
+                aspRatio = w/float(h)
+                if (aspRatio > 0.95 and aspRatio<1.05):
+                    objectType = "Square"
+                else:
+                    objectType = "Rect"
+            elif objCor > 4:
+                objectType = "Circle"
+            else:
+                objectType = "none"
+
+            cv2.rectangle(imgContour, (x, y), (x+w, y+h), (0,0,0), 3)
+            cv2.putText(imgContour, objectType,
+                        (x+(w//2)-50,y+(h//2)-10), cv2.FONT_HERSHEY_COMPLEX, 0.75,
+                        (0,0,0), 2)
 
 img = cv2.imread("Resources/shapes.png")
 imgContour = img.copy()
@@ -56,7 +75,7 @@ imgBlur = cv2.GaussianBlur(imgGray, (7,7), 1)   # Higher the value of sigmaX, hi
 imgCanny = cv2.Canny(imgBlur, 500, 350)
 getContours(imgCanny)
 
-imgStack = stackImages(0.5, ([img, imgGray, imgBlur],
+imgStack = stackImages(0.6, ([img, imgGray, imgBlur],
                              [imgBlank, imgCanny, imgContour]))
 
 cv2.imshow("Stacked Images", imgStack)
